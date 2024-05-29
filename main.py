ff518e491edc279190d6882f30adfee9d198f26d
Chimaroke <chimarokeonyebi@gmail.com>
2024-05-29 15:53:18 +0100

    Retrieve user posts data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
 from starlette.middleware import Middleware
 from middleware import TimeoutMiddleware
+from typing import Optional
 
 # Import the functions from the other scripts
 from profile_data import retrieve_profile_info
@@ -9,7 +10,7 @@ from user_post import retrieve_post_metrics
 from media import get_post_details
 
 app = FastAPI(middleware=[
-    Middleware(TimeoutMiddleware, timeout=60)
+    Middleware(TimeoutMiddleware, timeout=600)
 ])
 
 @app.get('/v1/api/profile')
@@ -26,17 +27,21 @@ def profile_info(username: str = Query(..., description="Username of the profile
 
 
 # Still needs work
-# @app.get('/v1/api/user_posts')
-# def user_posts(username: str = Query(..., description="Username to retrieve posts for")):
-#     try:
-#         posts = retrieve_post_metrics(username)
-#         if posts is None:
-#             raise HTTPException(status_code=404, detail="Posts for user not found")
-#         return JSONResponse(content=posts)
-#     except HTTPException as e:
-#         raise e
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+@app.get('/v1/api/user_posts')
+def user_posts(
+    username: str = Query(..., description="Username to retrieve posts for"),
+    from_date: Optional[str] = Query(None, description="Start date for post retrieval (YYYY-MM-DD)"),
+    to_date: Optional[str] = Query(None, description="End date for post retrieval (YYYY-MM-DD)")
+):
+    try:
+        posts = retrieve_post_metrics(username, from_date, to_date)
+        if not posts:
+            raise HTTPException(status_code=404, detail="Posts for user not found")
+        return JSONResponse(content=posts)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 
